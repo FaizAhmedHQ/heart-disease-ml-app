69dcd2a49c165c12b59194c65365ae44366ced41
FaizAhmedHQ <faizahmedkhan1503@gmail.com>
2026-02-13 18:28:46 +0530

    fix the the backend access

--- frontend/app.py
+++ frontend/app.py
@@ -137,7 +137,7 @@ with st.container():
         }
 
         res = requests.post(
-            "http://127.0.0.1:8000/predict",
+            "https://heart-disease-ml-app-mgbt.onrender.com/predict",
             json=payload
         )
 
